d69fee432e507dcc539c785cf208c7c80c33e8b4
allowedcloud <79382276+allowedcloud@users.noreply.github.com>
2022-12-28 16:25:34 -0500

    Add configuration for Vercel

--- components.d.ts
+++ components.d.ts
@@ -13,7 +13,6 @@ declare module '@vue/runtime-core' {
     FooterTopSection: typeof import('./src/components/FooterTopSection.vue')['default']
     FormStep: typeof import('./src/components/FormStep.vue')['default']
     FormWizard: typeof import('./src/components/FormWizard.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     HomeBottomQuoteButton: typeof import('./src/components/HomeBottomQuoteButton.vue')['default']
     HomeCheckSocial: typeof import('./src/components/HomeCheckSocial.vue')['default']
     HomeGoogleReviews: typeof import('./src/components/HomeGoogleReviews.vue')['default']
@@ -25,11 +24,6 @@ declare module '@vue/runtime-core' {
     HomeVideo: typeof import('./src/components/HomeVideo.vue')['default']
     HomeWhatSeparates: typeof import('./src/components/HomeWhatSeparates.vue')['default']
     HomeWhatSeparatesItem: typeof import('./src/components/HomeWhatSeparatesItem.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     QuoteForm: typeof import('./src/components/QuoteForm.vue')['default']
     ResidentialIntro: typeof import('./src/components/ResidentialIntro.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
@@ -37,8 +31,6 @@ declare module '@vue/runtime-core' {
     TheFooter: typeof import('./src/components/TheFooter.vue')['default']
     TheGetQuoteButton: typeof import('./src/components/TheGetQuoteButton.vue')['default']
     TheTopSection: typeof import('./src/components/TheTopSection.vue')['default']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
     TopWarningBox: typeof import('./src/components/TopWarningBox.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
