888c7113217727712b9fdf769868bdf413527739
allowedcloud <79382276+allowedcloud@users.noreply.github.com>
2022-12-30 06:06:48 -0500

    Add junk and packing pages

---
+++ src/views/junk.vue
@@ -0,0 +1,12 @@
+<template>
+  <TheTopSection
+    header="Let us take care of the junk"
+    img-src="/images/home-declutter.jpg"
+    img-alt="A messy room"
+    img-pos="bottom"
+  >
+    Maybe you need a few items removed during your move, or you have a whole
+    house that needs cleared out. We're here to assist however possible.
+  </TheTopSection>
+  <TheGetQuoteButton />
+</template>
---
+++ src/views/packing.vue
@@ -0,0 +1,11 @@
+<template>
+  <TheTopSection
+    header="Packing is an art of its own"
+    img-src="/images/home-packing.jpg"
+    img-alt="Tractor trailer driving in front of mountains"
+    img-pos="bottom"
+  >
+    <!--  -->
+  </TheTopSection>
+  <TheGetQuoteButton />
+</template>
